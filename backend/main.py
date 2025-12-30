b0d4fd8e00043b9f256ff5177cd244a74b8bf37d
Amrita Sinha <amritasinhaofficial@gmail.com>
2025-12-30 14:31:34 +0530

    add exam submission endpoint using POST

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
 app = FastAPI()
+
+class ExamAttempt(BaseModel):
+    student_id: int
+    answers: List[int]
+    time_taken: List[float]
+
 @app.get("/")
 def root():
- return{"message": "backend is running successfully"}
+    return {"message": "Backend is running successfully"}
+
+@app.get("/status")
+def status():
+    return {"status": "Server is healthy"}
+
+@app.post("/submit-exam")
+def submit_exam(attempt: ExamAttempt):
+    return {
+        "message": "Exam submitted successfully",
+        "student_id": attempt.student_id,
+        "total_questions": len(attempt.answers)
+    }
